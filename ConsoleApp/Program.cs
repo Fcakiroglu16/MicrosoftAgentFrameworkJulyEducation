acaacefb5548951dc46113f65c3ac5fda3551d82
fcakiroglu16 <f-cakiroglu@outlook.com>
2026-07-23 19:27:19 +0300

    Add ToolApproval functionality and update Program.cs to integrate it

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -14,6 +14,7 @@ Console.WriteLine("Microsoft Extensions AI Chat Client");
 //await AllResponseContentAgent.RunAsync(agent);
 //await MultiModelContentAgent.RunAsync(agent);
 //await CvExtraction.RunAsync();
-#pragma warning disable OPENAI001
-await CodeInterpreterHostedTool.RunAsync();
-#pragma warning restore OPENAI001
+// #pragma warning disable OPENAI001
+// await CodeInterpreterHostedTool.RunAsync();
+// #pragma warning restore OPENAI001
+await ToolApproval.RunAsync();
