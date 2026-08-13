b93b0217cd2b687f648c99426939cb8ca2cc21ea
fcakiroglu16 <f-cakiroglu@outlook.com>
2026-08-13 11:36:35 +0300

    Update Dockerfile to expose port 8088, modify Program.cs for agent initialization, and adjust package references in SimpleAgentDeploy.csproj

--- SimpleAgentDeploy/Program.cs
+++ SimpleAgentDeploy/Program.cs
@@ -1,10 +1,11 @@
 ﻿
+using Azure.AI.Projects;
+using Azure.Identity;
+using Microsoft.Agents.AI;
 using Microsoft.Agents.AI.Foundry.Hosting;
 using Microsoft.Extensions.AI;
 using OpenAI;
 
-var projectEndpoint = new Uri("https://julyeducation.services.ai.azure.com/api/projects/proj-july-education");
-var deployment = "gpt-5-mini";
 
 
 var apiKey = Environment.GetEnvironmentVariable("OPEN_AI_KEY");
@@ -14,20 +15,24 @@ if (string.IsNullOrWhiteSpace(apiKey))
 var chatClient = new OpenAIClient(apiKey)
     .GetChatClient("gpt-4o")
     .AsIChatClient();
+var agent= chatClient.AsAIAgent(
+    instructions: "You are a helpful AI assistant.",
+    name: "my-agent");
 
 
-var agent=chatClient
-    .AsAIAgent(
-        instructions: "You are a professional and empathetic customer service agent.", name: "customer-agent");
 
+// var projectEndpoint = new Uri("https://julyeducation.services.ai.azure.com/api/projects/proj-july-education");
+// var deployment = "gpt-5-mini";
+
+// AIAgent agent = new AIProjectClient(projectEndpoint, new DefaultAzureCredential())
+//     .AsAIAgent(
+//         model: deployment,
+//         instructions: "You are a helpful AI assistant.",
+//         name: "my-agent");
 
 var builder = AgentHost.CreateBuilder(args);
-
-
 builder.Services.AddFoundryResponses(agent);
 builder.RegisterProtocol("responses", endpoints => endpoints.MapFoundryResponses());
 
 var app = builder.Build();
-
-
 app.Run();
